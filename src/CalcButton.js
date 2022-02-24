10320640e82ba22a40567d6d20aad098bb55efb6
Samip <callmesamip@gmail.com>
2022-02-24 14:57:36 +1100

    Last Working Solution

--- src/CalcButton.js
+++ src/CalcButton.js
@@ -1,63 +1,68 @@
 import {React,useState} from 'react';
+
 let addNumbers = "";
+const calcSymbols = ["/", "*", "-", "+"];
+
+const btns = [{
+  key: 'clear',
+  value: 'AC'
+}, {
+  key: 'clean',
+  value: 'C'
+}, {
+  key: 'divide',
+  value: '/'
+}, {
+  key: 'multi',
+  value: '*'
+}, {
+  key: 'seven',
+  value: '7'
+}, {
+  key: 'eight',
+  value: '8'
+}, {
+  key: 'nine',
+  value: '9'
+}, {
+  key: 'minus',
+  value: '-'
+}, {
+  key: 'four',
+  value: '4'
+}, {
+  key: 'five',
+  value: '5'
+}, {
+  key: 'six',
+  value: '6'
+}, {
+  key: 'plus',
+  value: '+'
+}, {
+  key: 'three',
+  value: '3'
+}, {
+  key: 'two',
+  value: '2'
+}, {
+  key: 'one',
+  value: '1'
+}, {
+  key: 'dot',
+  value: '.'
+}, {
+  key: 'zero',
+  value: '0'
+}, {
+  key: 'equalsSign',
+  value: '='
+}];
+
 export const CalcButton = ({displayFunc}) => {
-  const btns = [{
-    key: 'clear',
-    value: 'AC'
-  }, {
-    key: 'clean',
-    value: 'C'
-  }, {
-    key: 'divide',
-    value: '/'
-  }, {
-    key: 'multi',
-    value: '*'
-  }, {
-    key: 'seven',
-    value: '7'
-  }, {
-    key: 'eight',
-    value: '8'
-  }, {
-    key: 'nine',
-    value: '9'
-  }, {
-    key: 'minus',
-    value: '-'
-  }, {
-    key: 'four',
-    value: '4'
-  }, {
-    key: 'five',
-    value: '5'
-  }, {
-    key: 'six',
-    value: '6'
-  }, {
-    key: 'plus',
-    value: '+'
-  }, {
-    key: 'three',
-    value: '3'
-  }, {
-    key: 'two',
-    value: '2'
-  }, {
-    key: 'one',
-    value: '1'
-  }, {
-    key: 'dot',
-    value: '.'
-  }, {
-    key: 'zero',
-    value: '0'
-  }, {
-    key: 'equalsSign',
-    value: '='
-  }];
+
   const handleOnClick = (val) => {
-    const calcSymbols = ["/", "*", "-", "+"];
+    
 
 
 
@@ -68,23 +73,27 @@ export const CalcButton = ({displayFunc}) => {
         return;
       } else {
         if (val === "." && (!addNumbers.length === undefined) && (addNumbers.length - 1).includes(".")) return;
-        addNumbers = addNumbers + val;
-       displayFunc(addNumbers)
+        if(val==="=" && addNumbers.length===0){
+          return;
+        }else{
+          addNumbers = addNumbers + val;
+          displayFunc(addNumbers);
+        }
       }
     }
     if (val === "=") {
       let total=""; //initialising total
       total = addNumbers.slice(0, -1);
       total=eval(total);
+      addNumbers=total.toString();
       displayFunc(total);
       return;
+     
+     
     }
     if (val === "C"){
       addNumbers=addNumbers.slice(0,-2);
-      if(addNumbers===0){
-        console.log("zero");
-      }
-      displayFunc(addNumbers)
+      displayFunc(0)
       return addNumbers;
     } ;
     if (val === "AC"){
